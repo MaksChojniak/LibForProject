dc54d3ad6bd1cf56dd000732dfdfe6e4ab5914aa
Maks <maks.chojniak@gmail.com>
2021-12-23 14:44:39 +0100

    11

--- app/src/main/java/com/example/libforproject/Text.kt
+++ app/src/main/java/com/example/libforproject/Text.kt
@@ -3,8 +3,8 @@ package com.example.libforproject
 import android.content.Context
 import android.widget.Toast
 
-object Text {
-    fun show(context: Context){
+object Texttt {
+    fun showww(context: Context){
         Toast.makeText(context,"Hello Word",Toast.LENGTH_SHORT).show()
     }
 }
